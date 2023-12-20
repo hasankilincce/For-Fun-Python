58cda18b6fd12e6b7bc3b679fed854b3532ccc4d
Hasan Kılınç <119063341+hasankilincce@users.noreply.github.com>
2023-12-20 17:34:59 +0300

    Update rock.py

--- rock.py
+++ rock.py
@@ -3,17 +3,14 @@ import random
 import sys
 import math
 
-# Her nesne durmaksızın hareket edecek DONE
-# Çarpıştıkları zaman etrafa saçılma
-# Kenarlara çarpma halinde sekme ve oyuna devam etme
-# Birbirlerine değdikleri halde şekil değiştirme
-
 pygame.init()
 
-width, height = 1080, 720
+################################ Temel Ayarlar ###########################################
+width, height = 1080, 920
 obj_size = 20
-game_speed = 40
-
+game_speed = 45
+obj_num = 120
+##########################################################################################
 font = pygame.font.SysFont(None, 96)
 
 screen = pygame.display.set_mode((width, height))
@@ -21,16 +18,19 @@ pygame.display.set_caption("Taş-Kağıt-Makas")
 
 object_type = ["rock", "paper", "scissors"]
 
+# Renkler
 white = (255, 255, 255)
+black = (0, 0, 0)
 red = (255, 0, 0)
 blue = (0, 0, 255)
 green = (0, 255, 0)
-black = (0, 0, 0)
 
 objects = []
-obj_num = 0
+obj_counter = 0
 
-rockImg = pygame.image.load("moai.png")
+# Obje resimleri import etme
+
+rockImg = pygame.image.load("rock.png")
 paperImg = pygame.image.load("paper.png")
 scissorsImg = pygame.image.load("scissors.png")
 
@@ -38,17 +38,23 @@ rockImg = pygame.transform.scale(rockImg, (obj_size, obj_size))
 paperImg = pygame.transform.scale(paperImg, (obj_size, obj_size))
 scissorsImg = pygame.transform.scale(scissorsImg, (obj_size, obj_size))
 
+# Oyun sonu obje sayıları
+
 rock = 0
 paper = 0
 scissors = 0
 
+
+# Oyun kodları
+
 class GameObject:
-    def __init__(self, x, y, width, height):
+    def __int__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
-        self.speed = random.randint(1, 5)
-        self.angle = random.uniform(0, 360)
+        self.speed = random.randint(1, 5)  # obje hızı
+        self.angle = random.uniform(0, 360)  # objenin açısı
         self.type = random.choice(object_type)
 
+        # Obje türüne göre resim atama
         if self.type == "rock":
             self.image = rockImg
         elif self.type == "paper":
@@ -56,26 +62,27 @@ class GameObject:
         elif self.type == "scissors":
             self.image = scissorsImg
 
-        # Resim boyutunu ayarla
+        # resim boyutunu ayarlama
         if self.image:
             self.image = pygame.transform.scale(self.image, (width, height))
 
+    # Hareket fonksiyonu
     def move(self):
-        # Obje hareket vektörlerini hesapla
+        # obje hareket vektörünü hesaplama
         dx = self.speed * math.cos(math.radians(self.angle))
         dy = self.speed * math.sin(math.radians(self.angle))
 
-        # Obje hareket vektörlerini uygula
+        # obje hareket vektörünü uygula
         self.rect.x += dx
         self.rect.y += dy
 
-        # Obje duvarlardan sekerse, hareket vektörünü tersine çevir
+        # Obje duvarlardan sekerse hareket yönünü tersine çevir
         if self.rect.left <= 0 or self.rect.right >= width:
             self.angle = 180 - self.angle
         if self.rect.top <= 0 or self.rect.bottom >= height:
             self.angle = -self.angle
 
-        # Duvarlara yapışma durumunu önlemek için objeyi düzelt
+        # Duvara yapışmasın önlemek için objeyi düzenle
         if self.rect.left < 0:
             self.rect.left = 0
         elif self.rect.right > width:
@@ -85,6 +92,7 @@ class GameObject:
         elif self.rect.bottom > height:
             self.rect.bottom = height
 
+    # Objeyi ekrana çizdirme
     def draw(self):
         if self.image:
             if self.type == "rock":
@@ -94,14 +102,17 @@ class GameObject:
             elif self.type == "scissors":
                 screen.blit(scissorsImg, self.rect)
 
+        # resim olmama durumunda renk ata
         else:
             if self.type == "rock":
                 pygame.draw.rect(screen, red, self.rect)
             elif self.type == "paper":
-                pygame.draw.rect(screen, green, self.rect)
-            elif self.type == "scissors":
                 pygame.draw.rect(screen, blue, self.rect)
+            elif self.type == "scissors":
+                pygame.draw.rect(screen, green, self.rect)
 
+
+# Rast gele obje oluşturma
 
 def create_random_object():
     return GameObject(
@@ -111,8 +122,10 @@ def create_random_object():
         obj_size
     )
 
+# Objelerin çarpışmasını kontrol etme
 def check_collision(obj1, obj2):
     return obj1.rect.colliderect(obj2.rect)
+
 
 def handle_collision(obj1, obj2):
     overlap = obj1.rect.clip(obj2.rect).width
@@ -125,9 +138,12 @@ def handle_collision(obj1, obj2):
         obj2.rect.x += offset_x
         obj2.rect.y += offset_y
 
+
+# Nesnelerin çarpışmaya göre dönüşmesi
 def object_changing(obj1, obj2):
     check = obj1.rect.colliderect(obj2.rect.inflate(20, 20))
 
+    # işi garantiye almak için bütün kombinasyonları yazmalyız
     if check:
         if obj1.type == "rock" and obj2.type == "paper":
             obj1.type = "paper"
@@ -137,27 +153,25 @@ def object_changing(obj1, obj2):
             obj1.type = "scissors"
         elif obj1.type == "paper" and obj2.type == "rock":
             obj2.type = "paper"
-        elif obj1.type == "scissors" and obj2.type == "rock":
-            obj1.type = "rock"
         elif obj1.type == "scissors" and obj2.type == "paper":
             obj2.type = "scissors"
+        elif obj1.type == "scissors" and obj2.type == "rock":
+            obj1.type = "rock"
 
 
-
+# Oyun Döngüsü
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    while obj_num < 120:
+    while obj_counter < obj_num:
         obj = create_random_object()
         objects.append(obj)
-        obj_num += 1
+        obj_counter += 1
 
     screen.fill(white)
-
-
 
     for obj in objects:
         obj.move()
@@ -171,17 +185,19 @@ while True:
         elif objects[i].type == "scissors":
             scissors += 1
 
-        for j in range(i + 1, len(objects)):
+        # Obje çarpışma ve değişme fonksiyonlarını uygulanması
+        for j in range(len(objects)):
             if check_collision(objects[i], objects[j]):
                 handle_collision(objects[i], objects[j])
             object_changing(objects[i], objects[j])
 
+    # Oyun içindeki nesnlerinin sayısına göre kazananın belirlenilmesi
     if (rock == 0) and (paper == 0) and (scissors > 0):
         end_text = font.render("Makas Kazandı", True, black)
         text_rect = end_text.get_rect(center=(width // 2, height // 2))
         screen.blit(end_text, text_rect)
         pygame.display.flip()
-        pygame.time.delay(5000)  # 5 saniye beklet
+        pygame.time.delay(5000)  # 5 saniye duraksama
         pygame.quit()
         sys.exit()
     elif (rock == 0) and (paper > 0) and (scissors == 0):
@@ -189,7 +205,7 @@ while True:
         text_rect = end_text.get_rect(center=(width // 2, height // 2))
         screen.blit(end_text, text_rect)
         pygame.display.flip()
-        pygame.time.delay(5000)  # 5 saniye beklet
+        pygame.time.delay(5000)  # 5 saniye duraksama
         pygame.quit()
         sys.exit()
     elif (rock > 0) and (paper == 0) and (scissors == 0):
@@ -197,13 +213,14 @@ while True:
         text_rect = end_text.get_rect(center=(width // 2, height // 2))
         screen.blit(end_text, text_rect)
         pygame.display.flip()
-        pygame.time.delay(5000)  # 5 saniye beklet
+        pygame.time.delay(5000)  # 5 saniye duraksama
         pygame.quit()
         sys.exit()
 
+    # Her döngüden sonra nesne sayacını sıfırla
     rock = 0
-    scissors = 0
     paper = 0
+    scissors = 0
 
     pygame.display.flip()
     pygame.time.Clock().tick(game_speed)
